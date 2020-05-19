b7bb095e0a57b3523c879c92e796b0fd0ef55d35
unknown <enosviniciusda@gmail.com>
2020-05-19 17:42:08 -0300

    criando opção de visualizar apenas tasks abertas

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react'
 import { View, Text, ImageBackground, StyleSheet, FlatList, TouchableOpacity, Platform } from 'react-native'
-
+import commonStyles from '../commonStyles'
 import todayImage from '../../assets/imgs/today.jpg'
-import commonStyle from '../commonStyles'
-import Task from '../components/Task'
-
 import Icon from 'react-native-vector-icons/FontAwesome'
-
 import moment from 'moment'
 import 'moment/locale/pt-br'
-
+import Task from '../components/Task'
 export default class TaskList extends Component {
-
     state = {
         showDoneTasks: true,
+        visibleTasks: [],
         tasks: [{
             id: Math.random(),
             desc: 'Comprar Livro de React Native',
             estimateAt: new Date(),
             doneAt: new Date(),
-        },
-        {
+        }, {
             id: Math.random(),
             desc: 'Ler Livro de React Native',
             estimateAt: new Date(),
@@ -28,33 +23,47 @@ export default class TaskList extends Component {
         }]
     }
 
+    componentDidMount = () => {
+        this.filterTasks()
+    }
+
     toggleFilter = () => {
-        this.setState({ showDoneTasks: !this.state.showDoneTasks })
+        this.setState({ showDoneTasks: !this.state.showDoneTasks }, this.filterTasks)
+    }
+
+    filterTasks = () => {
+        let visibleTasks = null
+        if(this.state.showDoneTasks) {
+            visibleTasks = [...this.state.tasks]
+        } else {
+            const pending = task => task.doneAt === null
+            visibleTasks = this.state.tasks.filter(pending)
+        }
+
+        this.setState({ visibleTasks })
     }
 
     toggleTask = taskId => {
         const tasks = [...this.state.tasks]
-
         tasks.forEach(task => {
-            if(task.id === taskId){
+            if(task.id === taskId) {
                 task.doneAt = task.doneAt ? null : new Date()
             }
         })
 
-        this.setState({ tasks })
+        this.setState({ tasks }, this.filterTasks)
     }
 
-    render(){
-        
-        const today = moment().locale('pt-br').format('ddd, D, [de] MMMM')
-
+    render() {
+        const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
             <View style={styles.container}>
-                <ImageBackground style={styles.background} source={todayImage}>
+                <ImageBackground source={todayImage}
+                    style={styles.background}>
                     <View style={styles.iconBar}>
-                        <TouchableOpacity onPress={this.toggleFilter} >
+                        <TouchableOpacity onPress={this.toggleFilter}>
                             <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
-                                size={20} color={commonStyle.colors.secundary}/>
+                                size={20} color={commonStyles.colors.secondary} />
                         </TouchableOpacity>
                     </View>
                     <View style={styles.titleBar}>
@@ -62,18 +71,15 @@ export default class TaskList extends Component {
                         <Text style={styles.subtitle}>{today}</Text>
                     </View>
                 </ImageBackground>
-                <View style={styles.taskContainer}>
-                    <FlatList data={this.state.tasks} 
+                <View style={styles.taskList}>
+                    <FlatList data={this.state.visibleTasks}
                         keyExtractor={item => `${item.id}`}
-                        renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask} />}/>
-
-
+                        renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask} />} />
                 </View>
             </View>
         )
     }
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1
@@ -81,24 +87,23 @@ const styles = StyleSheet.create({
     background: {
         flex: 3
     },
-    taskContainer: {
+    taskList: {
         flex: 7
     },
     titleBar: {
         flex: 1,
-        justifyContent: 'flex-end',
-
+        justifyContent: 'flex-end'
     },
     title: {
-        fontFamily: commonStyle.fontFamily,
-        color: commonStyle.colors.secundary,
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
         fontSize: 50,
         marginLeft: 20,
         marginBottom: 20
     },
     subtitle: {
-        fontFamily: commonStyle.fontFamily,
-        color: commonStyle.colors.secundary,
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
         fontSize: 20,
         marginLeft: 20,
         marginBottom: 30
@@ -109,4 +114,4 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end',
         marginTop: Platform.OS === 'ios' ? 40 : 10
     }
-})
+});
